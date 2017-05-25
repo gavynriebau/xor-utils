cf4e7b8e5c731d12ea676a27e94d4b81c913292d
Gavyn <gavyn.riebau@gmail.com>
2017-05-25 22:36:04 +0800

    Impl Xor for Read and bump version to 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -14,44 +14,53 @@ pub trait Xor {
     fn xor(&mut self, key_bytes : Vec<u8>) -> Vec<u8>;
 }
 
-impl<'a, R: Read> Xor for &'a mut R {
-    fn xor(&mut self, key_bytes : Vec<u8>) -> Vec<u8> {
+fn xor(reader: &mut Read, key_bytes : Vec<u8>) -> Vec<u8> {
+    let mut key_idx = 0;
+    let mut warning_shown = false;
+    let mut encoded_bytes: Vec<u8> = Vec::new();
 
-        let mut key_idx = 0;
-        let mut warning_shown = false;
-        let mut encoded_bytes: Vec<u8> = Vec::new();
+    // Iterate each chunk of input data and XOR it against the provided key.
+    loop {
+        let mut data = [0; 1024];
+        let num_read = reader.read(&mut data[..]).unwrap();
 
-        // Iterate each chunk of input data and XOR it against the provided key.
-        loop {
-            let mut data = [0; 1024];
-            let num_read = self.read(&mut data[..]).unwrap();
+        if num_read == 0 {
+            break;
+        }
 
-            if num_read == 0 {
-                break;
-            }
+        let data_bytes = &data[0 .. num_read];
 
-            let data_bytes = &data[0 .. num_read];
+        for b in data_bytes {
+            let k = key_bytes[key_idx];
+            let e = b ^ k;
 
-            for b in data_bytes {
-                let k = key_bytes[key_idx];
-                let e = b ^ k;
+            encoded_bytes.push(e);
 
-                encoded_bytes.push(e);
+            key_idx += 1;
 
-                key_idx += 1;
+            if key_idx >= key_bytes.len() {
+                key_idx = key_idx % key_bytes.len();
 
-                if key_idx >= key_bytes.len() {
-                    key_idx = key_idx % key_bytes.len();
-
-                    if !warning_shown {
-                        warning_shown = true;
-                        warn!("Key wasn't long enough and had to be re-used to fully encode data, use a longer key to be secure.");
-                    }
+                if !warning_shown {
+                    warning_shown = true;
+                    warn!("Key wasn't long enough and had to be re-used to fully encode data, use a longer key to be secure.");
                 }
             }
         }
+    }
 
-        encoded_bytes
+    encoded_bytes
+}
+
+impl<'a, R: Read> Xor for &'a mut R {
+    fn xor(&mut self, key_bytes : Vec<u8>) -> Vec<u8> {
+        xor(self, key_bytes)
+    }
+}
+
+impl Xor for Read {
+    fn xor(&mut self, key_bytes : Vec<u8>) -> Vec<u8> {
+        xor(self, key_bytes)
     }
 }
 
@@ -67,8 +76,7 @@ mod tests {
         let data : Vec<u8>  = vec![0b11111111u8, 0b11111111u8, 0b00001111u8, 0b10101010u8, 0b11111111u8, 0b11111111u8, 0b00001111u8, 0b10101010u8];
         let key : Vec<u8>   = vec![0b11111111u8, 0b00000000u8, 0b11110000u8, 0b01010101u8];
 
-        let mut input = Cursor::new(data);
-        let mut reader = &mut input as &mut Read;
+        let reader : &mut Read = &mut Cursor::new(data);
 
         let cipher = reader.xor(key);
 
