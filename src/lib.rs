8e38d30f6596ff5aa2854ffd19e7924787c3e912
Gavyn <gavyn.riebau@gmail.com>
2017-05-26 21:57:55 +0800

    Added gen_ascii_keys function

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,31 @@ impl Xor for Read {
     }
 }
 
+
+/// Generate all combinations of ASCII up to the supplied character length.
+///
+/// This can be used to get all the possible ASCII keys of a certain character length.
+pub fn gen_ascii_keys(length : u32) -> Vec<String> {
+    let mut keys : Vec<String> = Vec::new();
+    let max = 128u32.pow(length);
+
+    for i in 0..max {
+        let mut value = i;
+        let mut key = String::new();
+
+        for j in (0..length).rev() {
+            let digit = value / 128u32.pow(j);
+            value = value - digit * 128u32.pow(j);
+            key.push_str(format!("{}", (digit as u8) as char).as_str());
+        }
+
+        keys.push(key);
+    }
+
+    keys
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
